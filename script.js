15eedc6bd268f5e0b1becda43a4beb96b19096f1
HemanthChirikonda <67725821+HemanthChirikonda@users.noreply.github.com>
2020-07-02 12:13:02 +0530

    code to print from last to first with spaces

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 var new_string = "";
  
-for (var i = 0; i < 11; i++) {
- new_string += numsArr[i] 
+for (var i = 11; i >0; i -- ) {
+ new_string += numsArr[i-1] + " ";
 }
 console.log(new_string);
